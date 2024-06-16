a4179fc8e7a3a65d89c67f1e00c51ca8d637589d
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2024-06-16 11:57:32 +0300

    Add files via upload

---
+++ hw_06_06_2024/hw_06_06_2024/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace hw_06_06_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yevhenii Shostak\n11.08.2005\n");
+            Console.WriteLine("Country Moldova, City of Chisinau, Temporary residence ∙ major01510@gmail.com ∙ 0661124661 ∙\n\n");
+            Console.WriteLine("WORK EXPERIENCE\nNo experience in firms\n\n");
+            Console.WriteLine("Education\nStudent of the Odessa branch of “IT STEP” (now)\r\nSpecialty: Software Development\n\n");
+            Console.WriteLine("SKILLS\nLanguages: C++, JS\r\nTools: GitHub\r\nOther: S.O.L.I.D, OOP, Design Patterns\n\n");
+            Console.WriteLine("About\r\nAn aspiring software developer with a great desire\r\nto learn and work in a team environment\r\nStudying with an excellent performance in “IT STEP”\r\n" +
+                "I have a good understanding of OOP and S.O.L.I.D\r\n" +
+                "Languages: Ukrainian (fluent), Russian (fluent),\r\nEnglish (below average)\r\nLooking for a remote job (full time)\n\n");
+            Console.WriteLine("Links\nGitHub: https://github.com/Yevhenii81\r\nLinkedIn: https://www.linkedin.com/in/yevhenii-shostak-264176297/");
+        }
+    }
+}
